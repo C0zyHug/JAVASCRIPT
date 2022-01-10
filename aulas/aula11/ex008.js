755f218c317b9184fd701064c039079b9b9485e8
C0zyHug <97320458+C0zyHug@users.noreply.github.com>
2022-01-10 06:17:06 -0300

    Mais Javascript

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 55.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if(vel > 60) {
+    console.log('VAI TOMAR BAN')
+}
+
+console.log('Dirija usando o cinto de segurança.')
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'BRASIL'
+
+if (pais == 'BRASIL') {
+    console.log('BR!')
+}
+else {
+    console.log('GRINGO!')
+}
